24f7e430dbb876257963ff7bc130c2fbe23c16e3
omarnassar-school <54950372+omarnassar-school@users.noreply.github.com>
2020-06-04 12:16:33 -0700

    fixing about page

--- js/mail-script.js
+++ js/mail-script.js
@@ -10,7 +10,7 @@
             e.preventDefault(); // prevent default form submit
 
             $.ajax({
-                url: 'mail.php', // form action url
+                url: 'contact_process.php', // form action url
                 type: 'POST', // form submit method get/post
                 dataType: 'html', // request type html/json/xml
                 data: form.serialize(), // serialize form data
@@ -21,7 +21,7 @@
                 success: function(data) {
                     alert.html(data).fadeIn(); // fade in response data
                     form.trigger('reset'); // reset form
-                    submit.attr("style", "display: none !important");; // reset submit button text
+                    submit.attr("style", "display: none !important"); // reset submit button text
                 },
                 error: function(e) {
                     console.log(e)
