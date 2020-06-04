72b8fad8b9058b7f8db9ffe8c5c44ab9d1727bdb
omarnassar-school <54950372+omarnassar-school@users.noreply.github.com>
2020-06-03 22:58:50 -0700

    fixing about page

--- contact_process.php
+++ contact_process.php
@@ -1,24 +1,26 @@
+<html>
+<body>
 <?php
 
     $to = "omariio1000@gmail.com";
-    $from = $_REQUEST['email'];
-    $name = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
-    $cmessage = $_REQUEST['message'];
+    $from = $_POST['email'];
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    //$number = $_POST['number'];         //you don't have a field called number
+    $cmessage = $_POST['message'];
 
-    $headers = "From: $from";
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $from . "\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    //$headers = "From: $from";
+	$headers = "From: " . $from; //. "\r\n";
+	//$headers .= "Reply-To: ". $from . "\r\n";
+	//$headers .= "MIME-Version: 1.0\r\n";
+	//$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
     $subject = "You have a message from your website!";
 
     $logo = 'img/logo.png';
     $link = 'https://omarpdxpics.com';
 
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
+	/*$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
 	$body .= "<table style='width: 100%;'>";
 	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
 	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
@@ -30,8 +32,17 @@
 	$body .= "<tr><td></td></tr>";
 	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
 	$body .= "</tbody></table>";
-	$body .= "</body></html>";
+	$body .= "</body></html>";*/
+    $body = $cmessage;
 
+	//$echo ($from); 
     $send = mail($to, $subject, $body, $headers);
+    
+    if($send)
+        echo '<div > Thanks for contacting us, will get back to you shortly!</div>';
+    else
+        echo '<div > Sorry something went wrong, try to contact us again later.</div>';
 
 ?>
+</body>
+</html>
